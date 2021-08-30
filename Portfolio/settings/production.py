7bd30416e29c28109750572f4434a14006ac2a56
codertjay <codertjay@gmail.com>
2021-08-30 23:31:36 +0100

    fixed static files

--- Portfolio/settings/production.py
+++ Portfolio/settings/production.py
@@ -4,16 +4,16 @@ from decouple import config
 # Todo change database
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('POSTGRESQL_DATABASE_NAME'),
-        'USER': config('POSTGRESQL_DATABASE_USER'),
-        'PASSWORD': config('POSTGRESQL_DATABASE_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('POSTGRESQL_DATABASE_NAME'),
+#         'USER': config('POSTGRESQL_DATABASE_USER'),
+#         'PASSWORD': config('POSTGRESQL_DATABASE_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
 PARENT_HOST = '104.131.111.54:8000'
 DEFAULT_HOST = "www"
 DEFAULT_REDIRECT_URL = "http://104.131.111.54:8000"
