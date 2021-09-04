50e8ca6276adf3e7f833af1eeee9f915170eb137
codertjay <codertjay@gmail.com>
2021-09-04 11:14:20 +0100

    fixed static files

--- Portfolio/settings/__init__.py
+++ Portfolio/settings/__init__.py
@@ -1,6 +1,6 @@
 from decouple import config
-from .production import *
-# if config('DEBUG') == True:
-#     from .local import *
-# else:
-#     from .production import *
+
+if config('DEBUG') == True:
+    from .local import *
+else:
+    from .production import *
