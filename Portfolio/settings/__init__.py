525b106546550cc9c5687077f224bb37700e08c8
AFENIKHENA FAVOUR <63492224+codertjay@users.noreply.github.com>
2021-09-04 11:04:21 +0100

    Update __init__.py
    cf

--- Portfolio/settings/__init__.py
+++ Portfolio/settings/__init__.py
@@ -1,6 +1,6 @@
 from decouple import config
-
-if config('DEBUG') == True:
-    from .local import *
-else:
-    from .production import *
+from .production import *
+# if config('DEBUG') == True:
+#     from .local import *
+# else:
+#     from .production import *
