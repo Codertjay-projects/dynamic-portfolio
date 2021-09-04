a025ab6628a357d1a1ff8f072e84152e47aa8153
AFENIKHENA FAVOUR <63492224+codertjay@users.noreply.github.com>
2021-09-04 10:57:12 +0100

    Update production.py
    dd

--- Portfolio/settings/production.py
+++ Portfolio/settings/production.py
@@ -2,21 +2,21 @@ from .base import *
 from decouple import config
 
 # Todo change database
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('POSTGRESQL_DATABASE_NAME'),
-#         'USER': config('POSTGRESQL_DATABASE_USER'),
-#         'PASSWORD': config('POSTGRESQL_DATABASE_PASSWORD'),
-#         'HOST': 'localhost',
-#         'PORT': 5432,
-#     }
-# }
-PARENT_HOST = '104.131.111.54:8000'
+Database
+https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('POSTGRESQL_DATABASE_NAME'),
+        'USER': config('POSTGRESQL_DATABASE_USER'),
+        'PASSWORD': config('POSTGRESQL_DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
+PARENT_HOST = '52.35.31.177'
 DEFAULT_HOST = "www"
-DEFAULT_REDIRECT_URL = "http://104.131.111.54:8000"
+DEFAULT_REDIRECT_URL = "http://52.35.31.177"
 
 PAYSTACK_LIVE_KEY = config('PAYSTACK_LIVE_KEY', default='')
 PAYSTACK_PUBLIC_KEY = config('PAYSTACK_PUBLIC_KEY', default='')
