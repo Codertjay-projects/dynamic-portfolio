2acbbf5876958037ad157a4c755b62b665b6f444
codertjay <codertjay@gmail.com>
2021-09-12 03:23:23 +0100

    fixing services

--- Portfolio/settings/local.py
+++ Portfolio/settings/local.py
@@ -19,3 +19,28 @@ PARENT_HOST = '.localhost:8000'
 DEFAULT_HOST = "www"
 DEFAULT_REDIRECT_URL = "http://www.localhost:8000"
 
+
+# S3 BUCKETS CONFIG
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+# General optimization for faster delivery
+AWS_IS_GZIPPED = True
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
